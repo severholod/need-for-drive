3100dffe023cde38db06b1d46feff6983dc47040
Sever <karapuchelo98@gmail.com>
2020-12-08 21:34:25 +0400

    all layout and functionalities of the first iteration v1.1

--- src/components/OrderStepsNav/OrderTitle.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const OrderTitle = () => {
-    return (
-        <nav className="steps"><div className="order-title">Заказ номер RU58491823</div></nav>
-    )
-}
